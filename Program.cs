74e4cb146c05b09052db92cc8301e78f420a852e
fengyhack <fengyhack@gmail.com>
2014-10-28 10:19:39 +0800

    fengyhack

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UIShell.OSGi;
+
+namespace OSGi.HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using(BundleRuntime bdrt=new BundleRuntime())
+            {
+                Console.WriteLine("Start bundle...");
+                bdrt.Start();
+                bdrt.Stop();
+                Console.WriteLine("Press any key to continue...");
+                Console.ReadKey();
+            }
+        }
+    }
+}
