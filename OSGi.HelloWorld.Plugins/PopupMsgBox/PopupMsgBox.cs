1ee3ef6e268470b57970ec180df59e07bfdd3179
fengyhack <fengyhack@gmail.com>
2019-07-01 09:14:56 +0800

    examples collected

---
+++ OSGi.HelloWorld.Plugins/PopupMsgBox/PopupMsgBox.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Forms;
+using UIShell.OSGi;
+
+namespace PopupMsgBoxPlugin
+{
+    public class PopupMsgBox:IBundleActivator
+    {
+        public void Start(IBundleContext context)
+        {
+           MessageBox.Show("<Start> Hello World!","MsgBox");
+        }
+
+        public void Stop(IBundleContext context)
+        {
+            MessageBox.Show("<Stop> See you.","MsgBox");
+        }
+    }
+}
---
+++ OSGi.HelloWorld.Plugins/PrintMessage/PrintMessage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UIShell.OSGi;
+
+namespace PrintMessagePlugin
+{
+    public class PrintMessage:IBundleActivator
+    {
+        public void Start(IBundleContext context)
+        {
+            Console.WriteLine("<Start> Hello World  -- PrintMessage");
+        }
+
+        public void Stop(IBundleContext context)
+        {
+            Console.WriteLine("<Stop> See you next time. -- PrintMessage");
+        }
+
+    }
+}
